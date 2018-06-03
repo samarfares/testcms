147db2b966697b67c8935a8cc9346a4d3151794b
samarFares <samarFares@github.com>
2018-06-03 14:26:46 +0300

    first config & add user

---
+++ config.py
@@ -0,0 +1,19 @@
+from errno import errorcode
+
+import mysql.connector
+
+try:
+ cnx = mysql.connector.connect(user='root', password='alamarFoods2018',
+                              host='127.0.0.1',
+                              database='xyz')
+ cursor = cnx.cursor()
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cursor.close()
+    cnx.close()
